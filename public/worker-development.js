907e4428a5ccbeb0ce64284a7a7b7bd1c68169c1
Amanda <amandafanny200@gmail.com>
2023-12-28 23:12:19 +0800

    feat: add @types/react-dom

---
+++ public/worker-development.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// Cache the fallback HTML during installation.
+self.addEventListener("install", event => {
+  console.log("Service worker installed");
+});
+/******/ })()
+;
